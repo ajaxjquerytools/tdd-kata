7264169223b9351c6396dc0688cb741484f626d7
Volodymyr Dvornyk <volodymyr.dvornyk@keystonett.com>
2015-12-24 12:08:54 +0200

    rebase test

--- src/main/java/org/tdd/kata1/Product.java
+++ src/main/java/org/tdd/kata1/Product.java
@@ -12,6 +12,10 @@ public class Product implements IProduct {
         this.name=name;
         this.type = type;
         this.price = price;
+        System.out.println("This is rebase test 1");
+        System.out.println("This is rebase test 2");
+        System.out.println("This is rebase test 3");
+        System.out.println("This is rebase test 4");
     }
 
     public ProductType getType() {
