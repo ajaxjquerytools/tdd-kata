85b4fe6fcde0b34626db1f506845d753fe5943a9
Volodymyr Dvornyk <volodymyr.dvornyk@keystonett.com>
2015-12-24 12:31:25 +0200

    commit

--- src/main/java/org/tdd/kata1/PriceRule.java
+++ src/main/java/org/tdd/kata1/PriceRule.java
@@ -4,6 +4,9 @@ package org.tdd.kata1;
  * Created by dvornykv on 3/12/14.
  */
 public class PriceRule {
+    public PriceRule(){
+        System.out.println("Public constructor");
+    }
 
     static {
         System.out.println("This is static block");
